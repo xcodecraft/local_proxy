abaeacadebd91bb071ea825aceddab2ed15e47d5
ayb-zuowenjian <zuowenjian@ayibang.com>
2016-04-07 00:15:58 +0800

    Delete init.sh

--- _prj/init.sh
+++
@@ -1,3 +0,0 @@
-export PATH=.:/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/usr/local/bin
-/home/q/tools/pylon_rigger/rigger   conf -e dev -s cache,test,console
-/home/q/tools/pylon_rigger/rigger   restart 
